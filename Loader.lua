b85f865e396a34ca14238659116803193ed0bc14
xnkq <lordreligion@gmail.com>
2025-07-21 11:51:44 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,10 @@
 local GameId = game.GameId
 if GameId == 7074860883 then
-  loadstring(game:HttpGet("https://github.com/aw4e/Cheese/raw/dev/Game/AC.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xnkq/AC/refs/heads/main/AC.lua"))()
+elseif GameId == 7314989375 then
+  loadstring(game:HttpGet("https://github.com/aw4e/Cheese/raw/dev/Game/H.lua"))()
+elseif GameId == 7095682825 then
+  loadstring(game:HttpGet("https://github.com/aw4e/Cheese/raw/dev/Game/B.lua"))()
+elseif GameId == 7436755782 then
+  loadstring(game:HttpGet("https://github.com/aw4e/Cheese/raw/dev/Game/GaG.lua"))()
 end
